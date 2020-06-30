51ce63c51222adbae4766b60ebe90ba40e08f094
Joshua Lebert <expertjosh2000@gmail.com>
2020-06-30 16:18:37 -0400

    completed the purge command

---
+++ commands/purge.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'purge',
+    description: 'Deletes the specified amount of messages from the channel the command is sent in.',
+    aliases: ['prune'],
+    type: 'moderation',
+    usage: '<amount of messsages>',
+    args: true,
+    cooldown: 10,
+    permissions: 'Staff',
+    execute(message, args) {
+        message.delete();
+        message.channel.bulkDelete(args[0]);
+    }
+}
