dd07affc7a2c354efc5fa29a468bf1bc4677f571
Joshua Lebert <expertjosh2000@gmail.com>
2020-08-07 21:08:57 -0400

    Added the challenge feature

--- commands/ttt.js
+++ commands/ttt.js
@@ -8,7 +8,7 @@ module.exports = {
     type: 'misc',
     usage: '<argument or help>',
     args: true,
-    cooldown: 10,
+    cooldown: 2, // make 5-10 when done testing
     permissions: false,
     execute(message, args) {
         let cmdName = this.name;
@@ -26,11 +26,11 @@ module.exports = {
             };
 
             cmds.push(obj);
-        }
+        };
 
         function capitalizeFirstLetter(string) {
             return string.charAt(0).toUpperCase() + string.slice(1);
-          }
+        };
 
         function sendHelpEmbed(message) {
             let embed = new Discord.MessageEmbed;
@@ -42,7 +42,66 @@ module.exports = {
             embed.setDescription(str);
 
             message.channel.send(embed);
-        }
+        };
+
+        function startChallenge(msg) {
+            if (!msg.mentions.members.first()) {
+                message.channel.send('Please mention the user you would like to challenge.');
+            }
+            let challenger = msg.author;
+            let challenged = msg.mentions.users.first();
+
+            let embed = new Discord.MessageEmbed(); // This embed is just changed to contain what it needs to be before each message.
+            // create a collector for the challenged user
+            embed.setDescription(`${challenged.username}, you have been challenged to a match of Tic-Tac-Toe by ${challenger.username}.\n` +
+            `To accept, type 'accept'. to decline, type 'decline'.\n` +
+            `This challenge will expire in 30 seconds.`);
+
+            msg.channel.send(embed);
+
+            let filter = m => m.author === challenged;
+            let challengeResponse = msg.channel.createMessageCollector(filter, { time: 30000 });
+
+            let receivedResponse = false;
+            
+            challengeResponse.on('collect', m => {
+
+                let answer = m.content.toLowerCase();
+
+                if (!answer === 'accept' || !answer === 'decline') {
+                    return;
+                };
+
+                switch (answer.toLowerCase()) {
+                    case 'accept':
+                        receivedResponse = true;
+                        embed.setDescription(`${challenged.username} has accepted the challenge. The match will begin shortly.`);
+                        msg.channel.send(embed);
+                        startMatch();
+                        challengeResponse.stop('Answer collected.');
+                        break;
+                    case 'decline':
+                        receivedResponse = true;
+                        embed.setDescription(`${challenged.username} has decline the challenge.`);
+                        msg.channel.send(embed);
+                        challengeResponse.stop('Answer collected.');
+                        break;
+                };
+            });
+
+            challengeResponse.on('end', () => {
+                if (receivedResponse === false) {
+                    embed.setDescription(`The challenge has expired. If you wish to play a match, please challenge again.`);
+                    msg.channel.send(embed);
+                };
+            });
+
+
+        };
+
+        function startMatch() {
+
+        };
 
         let choice = args[0];
         switch (choice) {
@@ -50,7 +109,7 @@ module.exports = {
                 sendHelpEmbed(message, commands);
                 break;
             case "challenge":
-
+            startChallenge(message, args);
                 break;
             default:
                 message.channel.send('That is not a valid argument. Please use the \'help\' argument to see other availible arguments.');
